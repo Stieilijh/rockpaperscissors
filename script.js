18578196b28b7187220c2538ea50889010a16b09
Stieilijh <saitushar02@gmail.com>
2022-04-09 20:06:47 +0530

    Finish ui

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ function makeCpuChoice(){
     } 
 }
 function assignWinner(playerChoice,cpuChoice){
+    document.getElementById("cpuChoice").textContent=
+    "Cpu chooses "+cpuChoice;
     let player = playerChoice.toUpperCase();
     let cpu = cpuChoice.toUpperCase();
-    console.log(player+"   "+cpu);
+    
     if(player=="ROCK"){
         if(cpu=="SCISSORS"){
             return 1;
@@ -48,28 +50,63 @@ function assignWinner(playerChoice,cpuChoice){
 }
 function announceWinner(playerChoice,cpuChoice){
     let winner = parseInt(assignWinner(playerChoice,cpuChoice));
-    console.log(winner);
+    const playerPointsText=document.getElementById("pPoints");
+    const cpuPointsText=document.getElementById("cPoints");
+    const winnerMsg = document.getElementById("winner");
+    const reset = document.getElementById("resetButton");
+    const buttons = document.querySelectorAll("button");
     if(winner==1){
-        alert("Player wins! "+playerChoice+" beats "+cpuChoice);
+        winnerMsg.textContent="Player wins! "+playerChoice+" beats "+cpuChoice;
+        playerPointsText.textContent=parseInt(playerPointsText.textContent)+1;
     }else if(winner==-1){
-        alert("Cpu wins! "+playerChoice+" loses to "+cpuChoice);
+        winnerMsg.textContent="Cpu wins! "+playerChoice+" loses to "+cpuChoice;
+        cpuPointsText.textContent=parseInt(cpuPointsText.textContent)+1;
     }else if(winner==0){
-        alert("Draw! "+playerChoice+" draws to "+cpuChoice);
+        winnerMsg.textContent="Draw! "+playerChoice+" draws to "+cpuChoice;
     }else{
         console.log("Should not come here");
     }
-}
-function play(){
-    let playerChoice=prompt("Choose one : Rock , Paper or Scissors");
-    while(!(playerChoice.toUpperCase()=="ROCK"||
-    playerChoice.toUpperCase()=="PAPER"||
-    playerChoice.toUpperCase()=="SCISSORS")){
-        playerChoice=prompt
-        ("There must have been some typo please type the correct spelling");
+    if(cpuPointsText.textContent==5||playerPointsText.textContent==5){
+        let win =cpuPointsText.textContent==5?"Computer":"Player"; 
+        document.getElementById("final-winner").textContent="Match over !! " 
+        +win+" won the match!!";
+        reset.style.visibility="visible";
+        buttons.forEach((button)=>{
+            if(button.id!="resetButton")button.disabled=true;
+        });
     }
-    let cpuChoice=makeCpuChoice();
-    announceWinner(playerChoice,cpuChoice);
 }
-for(let i=0;i<5;i++)
-play();
+function play(id){
+    let playerChoice=id;
+    let cpuChoice = makeCpuChoice();
+    announceWinner(playerChoice,cpuChoice);
+    
+}
+
+window.onload=function(){
+    const buttons = document.querySelectorAll("button");
+    let playerChoice = "";  
+    buttons.forEach((button) => {
+            button.addEventListener('click', () => {
+            if(button.id!="resetButton")play(button.id);
+        });
+    });
+    const reset = document.getElementById("resetButton");
+    reset.addEventListener('click',()=>{
+        const playerPointsText=document.getElementById("pPoints");
+        const cpuPointsText=document.getElementById("cPoints");
+        const winnerMsg = document.getElementById("winner");
+        winnerMsg.textContent="";
+        playerPointsText.textContent="0";
+        cpuPointsText.textContent="0";
+        buttons.forEach((button)=>{
+            button.disabled=false;
+        });
+        document.getElementById("final-winner").textContent=
+        "";
+        document.getElementById("cpuChoice").textContent=
+    "";
+    document.getElementById("resetButton").style.visibility="hidden";
+    });
+}
 
